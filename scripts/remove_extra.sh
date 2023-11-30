0471e67ff40ff50687d83578e6a2d2515ee63ece
Calvin Behling <calvin.behling@gmail.com>
2023-11-30 10:31:49 -0600

    update script to remove extra s3 objects from mirror

--- scripts/remove_extra.sh
+++ scripts/remove_extra.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
 set -euo pipefail
 CONFIG='./.s3cfg'
-BUCKET='s3://warpsys/mirror'
+BUCKET='s3://warpsys/warehouse'
 BUCKET_LS_FILE='bucket_ls'
 WARE_ID_FILE='ware_ids'
 # uncomment to refresh bucket data
 # s3cmd -c "$CONFIG" ls --recursive "$BUCKET" > $BUCKET_LS_FILE
 
-BUCKET_HASH=$(column < "${BUCKET_LS_FILE}" --table-columns=date,time,size,object -J | jq -r .table[].object | cut -d/ -f6 | sort) 
->&2 echo "BUCKET_HASH"
->&2 echo "$BUCKET_HASH"
+OBJ_PATHS=$(column -t -o$'\t' < "${BUCKET_LS_FILE}" | cut -d$'\t' -f4 | sort)
+OBJ_HASH=$(echo "$OBJ_PATHS" | xargs -I_ basename _ | sort)
+>&2 printf 'OBJECTS: %d\n' $(echo "$OBJ_HASH" | grep -v '^$' | wc -l)
+
 
 WARE_HASH=$(cut < "${WARE_ID_FILE}" -d\: -f2)
->&2 echo 'WARE_HASH'
->&2 echo "$WARE_HASH"
-set -x
-EXTRA_HASH=$(diff <(echo "$WARE_HASH") <(echo "$BUCKET_HASH") | grep '>' | cut -d\  -f3)
->&2 echo "EXTRA_HASH"
->&2 echo "$EXTRA_HASH"
+>&2 printf 'WARES: %d\n' $(echo "$WARE_HASH" | grep -v '^$' | wc -l)
+
+EXTRA_HASH=$(comm -13 <(echo "$WARE_HASH") <(echo "$OBJ_HASH"))
+>&2 printf 'EXTRA OBJECTS: %d\n' $(echo "$EXTRA_HASH" | grep -v '^$' | wc -l)
 if [[ -z "$EXTRA_HASH" ]]; then 
     >&2 echo 'no extras to remove'
      exit 0
 fi
 
-REMOVE_OBJ=$(grep -F -f <(echo "$EXTRA_HASH") "$BUCKET_LS_FILE" | column -t -o$'\t' | cut -d$'\t' -f4)
+REMOVE_OBJ=$(grep -F -f <(echo "$EXTRA_HASH") <(echo "$") | column -t -o$'\t' | cut -d$'\t' -f4)
 >&2 echo "REMOVE_OBJ"
 >&2 echo "$REMOVE_OBJ"
 
